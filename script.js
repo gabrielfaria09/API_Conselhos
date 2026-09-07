e36370518ab7f79ae2234b29dbf0a7487e562256
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-09-07 14:44:52 -0300

    Polimento de experiência do usuário, com try..catch para exibir erro se necessário, e desabilitar botão durante requisição.

--- script.js
+++ script.js
@@ -2,18 +2,30 @@ const botao = document.getElementById("botao");
 const paragrafo = document.getElementById("conselho");
 
 async function aconselhar() {
+
+    botao.disabled = true;
+    botao.textContent = 'Carregando...';
+
+
+    try {
         const resposta = (await fetch('https://api.adviceslip.com/advice'));
 
         if(!resposta.ok){
-            console.log("error");
-            paragrafo.textContent = "Error";
+            throw new Error(`Erro na API: Status ${response.status}`);
         }
+
         else{
-        const dados = await resposta.json();
-        const texto = dados.slip.advice;
-        console.log(texto);
-        paragrafo.textContent = texto;
+            const dados = await resposta.json();
+            const texto = dados.slip.advice;
+            console.log(texto);
+            paragrafo.textContent = texto;
         }
+    } catch (error) {
+         console.error("Falha ao buscar conselho:", error.message);
+    }
+
+    botao.disabled = false;
+    botao.textContent = "Gerar conselho?";
 
 }
 botao.addEventListener('click', aconselhar);
