5bfdf9e8c41710e7f181189a90983c7a6b7dad79
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-09-07 13:50:38 -0300

    Adição de estrutura de condição por segurança.

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const paragrafo = document.getElementById("conselho");
 
 async function aconselhar() {
         const resposta = (await fetch('https://api.adviceslip.com/advice'));
+
+        if(!resposta.ok){
+            console.log("error");
+            paragrafo.textContent = "Error";
+        }
+        else{
         const dados = await resposta.json();
         const texto = dados.slip.advice;
         console.log(texto);
         paragrafo.textContent = texto;
+        }
 
 }
 botao.addEventListener('click', aconselhar);
