94bbe9ea46a7291a589984f0c0da74f41fe7ec96
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-09-07 13:40:17 -0300

    Conselho gerado com sucesso.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const botao = document.getElementById("botao");
+const paragrafo = document.getElementById("conselho");
+
+async function aconselhar() {
+        const resposta = (await fetch('https://api.adviceslip.com/advice'));
+        const dados = await resposta.json();
+        const texto = dados.slip.advice;
+        console.log(texto);
+        paragrafo.textContent = texto;
+
+}
+botao.addEventListener('click', aconselhar);
